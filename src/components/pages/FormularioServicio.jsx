ba5eb00b8aedc6a36e158a6d81ed8885ba11c56b
Valentina Iramain <valentinairamain2020@gmail.com>
2025-11-14 10:51:32 -0300

    se realiza maquetado de formulario servicio

--- src/components/pages/FormularioServicio.jsx
+++ src/components/pages/FormularioServicio.jsx
@@ -1,10 +1,155 @@
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
 
 const FormularioServicio = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
-        <div>
-            <p>formulario para crear y editar servicios</p>
-        </div>
+        <main className="container my-4">
+            <h1>Titulo</h1>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Servicio</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Ej: Diseño de sitio web institucional"
+                        {...register("servicio", {
+                            required: "El servicio es un dato obligatorio",
+                            minLength: {
+                                value: 5,
+                                message:
+                                    "El servicio debe contener como minimo 5 caracteres",
+                            },
+                            maxLength: {
+                                value: 100,
+                                message:
+                                    "El servicio debe contener como maximo 100 caracteres",
+                            },
+                        })}
+                    />
+                    <Form.Text className="text-danger">{errors.servicio?.message}</Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPrecio">
+                    <Form.Label>Precio*</Form.Label>
+                    <Form.Control
+                        type="number"
+                        placeholder="Ej: 50"
+                        // step={0.1}
+                        {...register("precio", {
+                            required: "El precio es un valor obligatorio",
+                            min: {
+                                value: 50,
+                                message:
+                                    "El precio minimo del producto debe ser de almenos $50",
+                            },
+                            max: {
+                                value: 1000000,
+                                message:
+                                    "El precio maximo de un producto debe ser de hasta $1000000",
+                            },
+                        })}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.precio?.message}
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formImagen">
+                    <Form.Label>Imagen URL*</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Ej: https://www.pexels.com/es-es/image/"
+                        {...register("imagen", {
+                            required: "La url de la imagen es un dato obligatorio",
+                            pattern: {
+                                value:
+                                    /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?(\.(jpg|jpeg|png|webp))$/,
+                                message:
+                                    "La imagen debe ser una url de imagen valida terminada en (jpg|jpeg|png|webp)",
+                            },
+                        })}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.imagen?.message}
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPrecio">
+                    <Form.Label>Categoría*</Form.Label>
+                    <Form.Select
+                        {...register("categoria", {
+                            required: "Debe seleccionar una categoria",
+                        })}
+                    >
+                        <option value="">Seleccione una opcion</option>
+                        <option value="Infusiones">Desarrollo Web</option>
+                        <option value="Batidos">Backend & API</option>
+                        <option value="dulce">Consultoría</option>
+                    </Form.Select>
+                    <Form.Text className="text-danger">
+                        {errors.categoria?.message}
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formImagen">
+                    <Form.Label>Descripción breve*</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Ej: Web profesional *responsive* con secciones clave: Inicio, Servicios, Quiénes Somos, Contacto y Blog (sin funcionalidad de publicación)"
+                        as="textarea"
+                        {...register("descripcion_breve", {
+                            required: "La descripción breve es un dato obligatorio",
+                            minLength: {
+                                value: 5,
+                                message: "La descrición breve debe tener almenos 5 caracteres",
+                            },
+                            maxLength: {
+                                value: 250,
+                                message:
+                                    "La descrición breve debe tener como máximo 250 caracteres",
+                            },
+                        })}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.descripcion_breve?.message}
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formImagen">
+                    <Form.Label>Descripción Amplia*</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Ej: Desarrollo de un sitio web corporativo **moderno y optimizado para SEO**. Incluye diseño personalizado basado en plantillas, integración de formulario de contacto, galería de imágenes, y hasta 7 páginas principales. Ideal para empresas que necesitan una fuerte presencia digital."
+                        as="textarea"
+                        rows={4}
+                        {...register("descripcion_amplia", {
+                            required: "La descripción amplia es un dato obligatorio",
+                            minLength: {
+                                value: 10,
+                                message:
+                                    "La descrición amplia debe tener almenos 10 caracteres",
+                            },
+                            maxLength: {
+                                value: 500,
+                                message:
+                                    "La descrición amplia debe tener como máximo 500 caracteres",
+                            },
+                        })}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.descripcion_amplia?.message}
+                    </Form.Text>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Enviar
+                </Button>
+            </Form>
+        </main>
     );
 };
 

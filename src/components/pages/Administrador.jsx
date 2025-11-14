18dbfd67fd318029c32d04faab84a1818c0c871e
Valentina Iramain <valentinairamain2020@gmail.com>
2025-11-14 10:23:44 -0300

    agreegamos el maquetado de la pagina del administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,36 +1,26 @@
 
-import Table from 'react-bootstrap/Table';
+import { Button, Table } from 'react-bootstrap';
+import ItemTabla from '../services/ItemTabla';
+
 const Administrador = () => {
     return (
-        <main>
-            <h1>Administrador Servicios</h1>
+        <main className='container my-4'>
+            <div className='d-flex justify-content-between align-items-center'>
+                <h1 className='mb-3'>Administrar Servicios</h1>
+                <Button>Crear</Button>
+
+            </div>
             <Table striped bordered hover>
                 <thead>
-                    <tr>
+                    <tr className='text-center'>
                         <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
+                        <th>Servicio</th>
+                        <th>Precio</th>
+                        <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                    <ItemTabla></ItemTabla>
                 </tbody>
             </Table>
         </main>
--- src/components/services/ItemTabla.jsx
+++ src/components/services/ItemTabla.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { Button } from "react-bootstrap";
+
 
 const ItemTabla = () => {
     return (
-        <div>
-            {/* aca va <tr></tr> */}
-        </div>
+        
+            <tr>
+                <td>1</td>
+                <td>e-commerce</td>
+                <td>$ 300.000</td>
+                <td className="text-center"><Button variant='warning' className="me-2"><i className="bi bi-pencil-fill"></i></Button>  <Button variant='danger'><i className="bi bi-trash3-fill"></i></Button></td>
+            </tr>
+        
     );
 };
 

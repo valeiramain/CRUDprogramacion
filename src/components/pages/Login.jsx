8b93899204c3dfe7349e4d2236211b71671755a7
Valentina Iramain <valentinairamain2020@gmail.com>
2025-11-14 10:49:24 -0300

    agregamos validaciones en el formulario de login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,28 +1,47 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+import { useForm } from "react-hook-form"
+
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
     return (
         <main className='container my-4'>
             <h1>Login</h1>
-            <Form>
+            <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email:</Form.Label>
-                    <Form.Control type="email" placeholder="ingrese email" />
-                    <Form.Text className="text-danger">
-                        Error de validación
-                    </Form.Text>
+                    <Form.Control type="email" placeholder="ingrese email" {...register('email', {
+                        required: 'El email es un dato obligatorio',
+                        pattern: {
+                            value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                            message: 'el email debe cumplir con el siguiente formato: email@dominio.extensión'
+                        }
+                    })} />
+                    <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Contraseña:</Form.Label>
-                    <Form.Control type="password" placeholder="Contraseña" />
-                    <Form.Text className="text-danger">
-                        Error de validación
-                    </Form.Text>
+                    <Form.Control type="password" placeholder="Contraseña"  {...register('password', {
+                        required: 'La contraseña es un dato obligatorio.',
+                        pattern: {
+                            value: /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                            message: 'La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter especial.'
+                        }
+                    })} />
+                    <Form.Text className="text-danger">{errors.password?.message}</Form.Text>
                 </Form.Group>
-               
+
                 <Button variant="primary" type="submit">
                     Enviar
                 </Button>

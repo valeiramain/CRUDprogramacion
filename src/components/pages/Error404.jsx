b27f62bc4c5831d15e69c9a63f3800ecb63ecec3
Valentina Iramain <valentinairamain2020@gmail.com>
2025-12-12 10:38:39 -0300

    se agrega imagen a error 404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,9 +1,14 @@
-
+import { Link } from 'react-router';
+import img404 from '../../assets/kostiantyn-li-Fi_nhg5itCw-unsplash.jpg'
+import { Image } from 'react-bootstrap';
 
 const Error404 = () => {
     return (
-        <div>
-            <p>Página no encontrada! error 404</p>
+        <div className="text-center">
+            <Link to='/'>
+                <Image src={img404} fluid alt="Error 404" className='w-50' />
+                <h3 className='text-danger fw-bold'>Oops! Página no encontrada!</h3>
+            </Link>
         </div>
     );
 };
